b8d9d7244b8068e3eea0118572335c147e1b793a
Ayushm74 <Ayushmishra74992@gmail.com>
2025-06-10 18:08:54 +0530

    UpdateFirebaseApi

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC7Z1K0sveSmBklxUDPCeImwzGBSiJZMIo",
-  authDomain: "nilewellness1.firebaseapp.com",
-  projectId: "nilewellness1",
-  storageBucket: "nilewellness1.firebasestorage.app",
-  messagingSenderId: "570005277635",
-  appId: "1:570005277635:web:d79fcfb9841b202e9d8b76"
+  apiKey: "AIzaSyC8ivdjElwzi1Lpdkf0YO9Cn3iI6WhyELo",
+  authDomain: "nilewellness1-70b3f.firebaseapp.com",
+  projectId: "nilewellness1-70b3f",
+  storageBucket: "nilewellness1-70b3f.firebasestorage.app",
+  messagingSenderId: "696188282218",
+  appId: "1:696188282218:web:8c5a74f1e3c482caa17113"
 };
 
 // Initialize Firebase
