33033f697302c9c35999f80455b4de91e012a5bb
Ayushm74 <Ayushmishra74992@gmail.com>
2025-07-23 20:20:19 +0530

    GoogleAds+Analytis

--- src/components/common/GoogleAnalytics.tsx
+++ src/components/common/GoogleAnalytics.tsx
@@ -2,23 +2,29 @@ import { useEffect } from 'react';
 
 const GoogleAnalytics = () => {
   useEffect(() => {
-    // Add Google Ads (gtag.js) script
+    // Add Google tag (gtag.js) - Google Ads + GA4 script
     const script1 = document.createElement('script');
     script1.async = true;
-    script1.src = 'https://www.googletagmanager.com/gtag/js?id=AW-17040751778';
+    script1.src = 'https://www.googletagmanager.com/gtag/js?id=G-2EM7PKDET3';
     document.head.appendChild(script1);
 
-    // Fix TypeScript error for window.dataLayer
-    (window as any).dataLayer = (window as any).dataLayer || [];
-    function gtag(...args: any[]) {
-      (window as any).dataLayer.push(args);
-    }
-    gtag('js', new Date());
-    gtag('config', 'AW-17040751778');
+    // Add inline script for gtag config
+    const script2 = document.createElement('script');
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      // Google Analytics
+      gtag('config', 'G-2EM7PKDET3');
+      // Google Ads
+      gtag('config', 'AW-17040751778');
+    `;
+    document.head.appendChild(script2);
 
     return () => {
       // Cleanup
       document.head.removeChild(script1);
+      document.head.removeChild(script2);
     };
   }, []);
 
